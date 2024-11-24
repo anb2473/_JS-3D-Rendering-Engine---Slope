27cb5a8563b2898c7b13e91dbe07d81d79908b3b
anb2473 <austinnabilblass@gmail.com>
2024-11-23 22:18:44 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -625,6 +625,7 @@ function drawRect(x1, y1, x2, y2, x3, y3, x4, y4, color){
     ctx.fill(path);
 
     ctx.lineCap = 'round';
+    ctx.lineJoin = 'round';
     ctx.strokeStyle = color;
     ctx.lineWidth = 5;
     ctx.stroke(path);
